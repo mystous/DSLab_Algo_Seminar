676cb1bb7ac491c92d1698cff75f0feef4493b7a
mandomong <hyeminkim@korea.ac.kr>
2015-03-03 09:58:18 +0900

    change

--- 001. 세미나 자료/02.2015/02. 2차시/20150226_HyeminKim_vector_multiplyer.cpp
+++ 001. 세미나 자료/02.2015/02. 2차시/20150226_HyeminKim_vector_multiplyer.cpp
@@ -3,12 +3,10 @@
 #include "TimeMeasurement.h"
 
 
-#define MAXSIZE 1024
+#define MAXSIZE 8192
 
 typedef struct cnum
 {
-	//double row;
-	//double col;
 	double realnum;
 	double imanum;
 }Cnum;
@@ -22,7 +20,6 @@ Cnum mult(Cnum num1,Cnum num2)
 	return result;
 }
 
-
 Cnum sum(Cnum num1,Cnum num2)
 {
 	Cnum result;
@@ -32,7 +29,7 @@ Cnum sum(Cnum num1,Cnum num2)
 	return result;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	FILE	*mat;
 	FILE	*vec;
@@ -47,13 +44,26 @@ int main(void)
 	double	Row=0, Col=0;
 	double	vec_row;
 
-	double  rc_arr[2];
+	double  mat_RC_data[6];
 
-	long mat_in=0;
+	long	mat_in=0;
+
+	int		j=0;
 
 	mat = fopen("matrix.dat","rb");
 	vec = fopen("vector_input.dat","rb");
 	end = fopen("vector_output.dat","wb");
+
+	
+	// exe input number 검사
+	if (argc == 4 && atoi(argv[1]) && atoi(argv[2]))
+		printf("input = %d x %d, Thread = %d \n",atoi(argv[1]), atoi(argv[2]),atoi(argv[3]));
+	else
+	{
+		fputs("[Error] Not a correct input", stderr);
+		exit(1);
+	}
+	
 
 	// 파일 유무 검사
 
@@ -72,18 +82,59 @@ int main(void)
 	start_time_measurement(); //시간측정 시작
 
 	// 매트릭스 크기
-	int point;
-	point = -4*sizeof(double);
 
-	if(fseek(mat, point, SEEK_END)==-1)
+	int point;
+
+	for(int i=3;i>0;i--)
 	{
-		printf("error\n");
+		point = i*4*sizeof(double);
+		point = -point;
+		if(fseek(mat, point, SEEK_END)==-1)
+		{
+			printf("error\n");
+		}
+		fread(&mat_RC_data[j],sizeof(double),2,mat);
+		Row=mat_RC_data[j]+1;
+		j++;
+		Col=mat_RC_data[j]+1;
+		j++;
+		printf("row : %f, col : %f\n",Row,Col);
 	}
 
-	fread(rc_arr,sizeof(double),2,mat);
+	/*
+	if(( (mat_RC_data[0]) != (mat_RC_data[2]) != (mat_RC_data[4]) ))
+	{
+	fputs("[ERROR] Matrix File ROW_DATA error", stderr);
+	exit(1);
+	}
+	if(mat_RC_data[1]+2!=mat_RC_data[3]+1!=mat_RC_data[5])
+	{
+	fputs("[ERROR] Matrix File COL_DATA error", stderr);
+	exit(1);
+	}
+	*/
 
-	Row=rc_arr[0]+1;
-	Col=rc_arr[1]+1;
+	// matrix.dat 행 열값 부분 오류 검사
+	if(mat_RC_data[4]-mat_RC_data[2]>1)
+	{
+		fputs("[ERROR] Matrix File ROW_DATA error", stderr);
+		exit(1);
+	}
+	if(mat_RC_data[2]-mat_RC_data[0]>1)
+	{
+		fputs("[ERROR] Matrix File ROW_DATA error", stderr);
+		exit(1);
+	}
+	if(mat_RC_data[5]-mat_RC_data[3]>2)
+	{
+		fputs("[ERROR] Matrix File ROW_DATA error", stderr);
+		exit(1);
+	}
+	if(mat_RC_data[3]-mat_RC_data[1]>2)
+	{
+		fputs("[ERROR] Matrix File ROW_DATA error", stderr);
+		exit(1);
+	}
 
 	printf("전체 열의 값 = %f\n전체 행의 값 = %f \n", Row, Col);
 
@@ -100,17 +151,14 @@ int main(void)
 	}
 
 	//행렬값 꺼내서 연산
-	int j=0;
+	j=0;
 	int button=0;
-
-	long fpos;
-
 
 	fseek(mat,0,SEEK_SET); //파일 포인터 위치 초기화
 	fseek(vec,0,SEEK_SET); //파일 포인터 위치 초기화
 	fseek(end,0,SEEK_SET); //파일 포인터 위치 초기화
 
-	for(int k=0; k<2000; k++)
+	for(int k=0; k<Row; k++)
 	{
 		//인덱스 초기화
 		button =0;
@@ -118,7 +166,7 @@ int main(void)
 		fseek(vec,0,SEEK_SET); //파일 포인터 위치 초기화
 
 		//mat 행렬 가져와서 mat_buf[]에 저장 100개씩 -> ////vec 행의 갯수에 따라로 수정해야함 //// 일단은 100개단위로
-		for(int i=0; i<400; i++)
+		for(int i=0; i<Col*4; i++)
 		{
 			fread(&main_buf[i], sizeof(double), 1, mat);
 			//printf("값 : %f ", main_buf[i]);
@@ -137,20 +185,11 @@ int main(void)
 			if(button==4)
 				button=0;
 		}
-	
-		/* mat index 체크 ,값 체크
-	mat_in = ftell(mat);
-	printf("mat 인덱스 값 = %d\n",mat_in);
-		
-	for(int i=0;i<100;i++)
-		printf("realnum : %f imanum : %f \n",mat_buf[i].realnum,mat_buf[i].imanum);
-
-		*/
 
 		//vec 행렬 가져와서 vec_buf[]에 저장
 		j=0;
 
-		for(int y=0; y<200; y++)
+		for(int y=0; y<Col*2; y++)
 		{
 			fread(&main_vec_buf[y],sizeof(double),1,vec);
 			if(y>1&&y%2==0)
@@ -161,62 +200,29 @@ int main(void)
 			{
 				vec_buf[j].imanum = main_vec_buf[y];
 			}			
-			
-			//printf("vec [%d] = 값 : %f\n ",y, main_vec_buf[y]);
 
 		}
-		
+
 
 
 		// mat * vec
 
-		for(int z=0; z<100; z++)
+		for(int i=0; i<Col; i++)
 		{
-			result[z]=mult(mat_buf[z],vec_buf[z]);
-			//printf("result [%d] : %f, %f\n",z,result[z].realnum,result[z].imanum);
+			result[i]=mult(mat_buf[i],vec_buf[i]);
 		}
 
-		for(int zy=1; zy<100; zy++)
+		for(int i=1; i<Col; i++)
 		{
-			result[0]=sum(result[0],result[zy]);			
+			result[0]=sum(result[0],result[i]);			
 		}
-
-
-		//최종결과값 출력 이수가 fwrite 된당
-		//printf("vec real [0] : %f ",result[0].realnum);
-		//printf("vec ima  [0] : %f\n",result[0].imanum);
 
 		fwrite(&result[0].realnum,sizeof(double),1,end);
 		fwrite(&result[0].imanum,sizeof(double),1,end);
 
-		printf("[%d] realnum : %f imanum : %f \n",k,result[0].realnum,result[0].imanum);
-
-		
-		/*
-		fclose(end);
-
-		end = fopen("vector_output.dat","rb");
-		double bbb[2];
-		fread(&bbb[0],sizeof(double),1,end);
-		fread(&bbb[1],sizeof(double),1,end);
-
-		//printf("realnum : %f imanum : %f \n",bbb[0],bbb[1]);
-		*/
-		
-
 
 	}
-
-	/*
-	for(int i=0;i<100;i++)
-	{
-	//vec_buf 값 표시
-	printf("vec real [%d] : %f ",i,vec_buf[i].realnum);
-	printf("vec ima  [%d] : %f\n",i,vec_buf[i].imanum);
-
-	}
-	*/
-
+	printf("Calculation Complete ");
 	end_time_measurement(); //시간 측정 끝
 
 	fclose(mat);
@@ -226,28 +232,13 @@ int main(void)
 
 	return 0;
 }
-
-/*
-int mat_index=3;
-
-fseek(mat,1,SEEK_SET); //파일 포인터 위치 초기화
-
-int fpos;
-fpos=ftell(mat);
-printf("fpos = %d\n",fpos);
-
-fseek(mat,3,SEEK_SET); //파일 포인터 위치 초기화
-
-fread(&main_buf[0], sizeof(double), 2, mat);
-printf("main_buf[0] = %f \n",main_buf[0]);
-fpos=ftell(mat);
-printf("fpos = %d\n",fpos);
-
-fseek(mat,4,SEEK_SET);
-
-fread(&main_buf[1], sizeof(double), 2, mat);
-printf("main_buf[1] = %f \n",main_buf[1]);
-fpos=ftell(mat);
-printf("fpos = %d\n",fpos);
-*/
-
+	/*
+	// exe input number 검사
+	if (argc == 4 && atoi(argv[1]) && atoi(argv[2]))
+		printf("input = %d x %d, Thread = %d \n",atoi(argv[1]), atoi(argv[2]),atoi(argv[3]));
+	else
+	{
+		fputs("[Error] Not a correct input", stderr);
+		exit(1);
+	}
+	*/
