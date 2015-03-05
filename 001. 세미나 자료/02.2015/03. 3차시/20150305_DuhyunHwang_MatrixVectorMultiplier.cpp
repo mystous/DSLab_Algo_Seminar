c04215cacbb3c7e2b45c302f3e45d21e40bccaf6
duhyun <doohh88@gmail.com>
2015-03-06 02:40:34 +0900

    대략 완료
    대략 완료

--- 001. 세미나 자료/02.2015/03. 3차시/20150305_DuhyunHwang_MatrixVectorMultiplier.cpp
+++ 001. 세미나 자료/02.2015/03. 3차시/20150305_DuhyunHwang_MatrixVectorMultiplier.cpp
@@ -15,14 +15,27 @@ POINTERS fp;
 SIZES size;
 BUF buf;
 P_IDX p_idx;
-//bool read_flag = false;
-//bool calc_flag = false;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char *argv[]){
 	pthread_t *threads = NULL;
+	void *(*func_p)(void *);
+
 	//pthread_t threads;
-	int thr_num = 2; //thread수 받기 설정해야함
+	//int thr_num = 32; //thread수 받기 설정해야함
+	int thr_num;
 	int status;
+	
+	if (argc == 5){
+		size.matrix.row = atof(argv[1]);
+		size.matrix.col = atof(argv[3]);
+		thr_num = atoi(argv[4]);
+	}
+	else{
+		printf("please input options");
+		printf("ex) .exe m x n thread_num");
+		exit(1);
+	}
 
 	//file open
 	open_file(&fp);
@@ -35,28 +48,33 @@ int main(int argc, char *argv[]){
 
 	//*****************read data**************************//
 	read_data(&fp, &size, &buf);
+		
+	//****************read by threads*******************// ==> 실패!!
+	/*free(buf.matrix);
+	free(buf.vector);
+	buf.matrix = (double *)malloc(sizeof(double) * size.matrix.elem_num * 4);
+	buf.vector = (double *)malloc(sizeof(double) * size.vector.elem_num * 2);
+	func_p = thread_read;
+	make_thread(&threads, thr_num, func_p);
+	join_thread(&threads, thr_num, &status); */
 
 	//*****************calc data**************************//
-	//calc_data(&fp, &size, &buf);
-
-
-
-
-	//*******************thread******************//
-	//make thread
-	make_thread(&threads, thr_num);
-	//join thread -> wait thread exit
+	calc_data(&fp, &size, &buf);
+	free(buf.output);
+	//*****************calc by threads******************//
+	func_p = thread_calc;
+	make_thread(&threads, thr_num, func_p);
 	join_thread(&threads, thr_num, &status);
-	
+
 	//txt 출력
 	FILE *fp_txt;
 	fopen_s(&fp_txt, "output.txt", "w");
 	for (int i = 0; i < size.output.elem_num; i++){
-		fprintf(fp_txt, "%d	:	%f	%f\n", i, buf.output[2*i], buf.output[2*i+1]);
-	}	
+		fprintf(fp_txt, "%d	:	%f	%f\n", i, buf.output[2 * i], buf.output[2 * i + 1]);
+	}
 	fclose(fp_txt);
 
-		
+
 	//free alloc
 	free(threads);
 	free_alloc(&buf);
@@ -138,7 +156,7 @@ void read_data(POINTERS *fp, SIZES* size, BUF *buf){
 	start_time_measurement();
 	buf->matrix = (double *)malloc(sizeof(double) * size->matrix.elem_num * 4);
 	_fseeki64(fp->matrix, 0, SEEK_SET);
-	fread(buf->matrix, sizeof(double), size->matrix.elem_num * 4 , fp->matrix);
+	fread(buf->matrix, sizeof(double), size->matrix.elem_num * 4, fp->matrix);
 	buf->vector = (double *)malloc(sizeof(double) * size->vector.elem_num * 2);
 	_fseeki64(fp->vector, 0, SEEK_SET);
 	fread(buf->vector, sizeof(double), size->vector.elem_num * 2, fp->vector);
@@ -146,17 +164,17 @@ void read_data(POINTERS *fp, SIZES* size, BUF *buf){
 }
 
 void calc_data(POINTERS *fp, SIZES* size, BUF *buf){
-	start_time_measurement();
 	buf->output = (double *)malloc(sizeof(double) * size->output.elem_num * 2);
+	memset(buf->output, 0, sizeof(double) * size->output.elem_num * 2);
+	start_time_measurement();
 	int matrix_idx, vector_idx, output_idx;
 	matrix_idx = 0;
-	memset(buf->output, 0, sizeof(double) * size->output.elem_num * 2);
 	for (output_idx = 0; output_idx < size->output.elem_num; output_idx++){
 		for (vector_idx = 0; vector_idx < size->vector.elem_num; vector_idx++){
 			buf->output[2 * output_idx] += real_calc(buf->matrix[4 * matrix_idx + 2], buf->matrix[4 * matrix_idx + 3],
-													buf->vector[2 * vector_idx], buf->vector[2 * vector_idx + 1]);
+				buf->vector[2 * vector_idx], buf->vector[2 * vector_idx + 1]);
 			buf->output[2 * output_idx + 1] += imag_calc(buf->matrix[4 * matrix_idx + 2], buf->matrix[4 * matrix_idx + 3],
-														buf->vector[2 * vector_idx], buf->vector[2 * vector_idx + 1]);
+				buf->vector[2 * vector_idx], buf->vector[2 * vector_idx + 1]);
 			matrix_idx++;
 		}
 	}
@@ -179,39 +197,45 @@ void free_alloc(BUF *buf){
 	free(buf->output);
 }
 
-void make_thread(pthread_t **threads, int thr_num){
-	int thr_id;
-	
-
-	*threads = (pthread_t *)malloc(sizeof(pthread_t)*5);
-	p_idx.quotient = size.matrix.row / thr_num;
+void make_thread(pthread_t **threads, int thr_num, void *(*func_p)(void*)){
 	//start measurement
-	start_time_measurement();	//측정
+
+	int thr_id;
+	*threads = (pthread_t *)malloc(sizeof(pthread_t)*thr_num);
+	p_idx.pt = (PT *)malloc(sizeof(PT)*thr_num);
+	p_idx.m_quotient = size.matrix.row / thr_num;
+	p_idx.n_quotient = size.vector.row / thr_num;
 	buf.output = (double *)malloc(sizeof(double) * size.output.elem_num * 2);	//할당
 	memset(buf.output, 0, sizeof(double) * size.output.elem_num * 2);	//초기화
 
+
+	start_time_measurement();	//측정
+	
+	
 	for (int i = 0; i < thr_num; i++){
-		p_idx.start_idx = i*p_idx.quotient; 
+		p_idx.pt[i].m_start_idx = i*p_idx.m_quotient;
+		p_idx.pt[i].n_start_idx = i*p_idx.n_quotient;
 		if (i != thr_num){
-			p_idx.end_idx = p_idx.start_idx + p_idx.quotient - 1;
+			p_idx.pt[i].m_end_idx = p_idx.pt[i].m_start_idx + p_idx.m_quotient - 1;
+			p_idx.pt[i].n_end_idx = p_idx.pt[i].n_start_idx + p_idx.n_quotient - 1;
 		}
 		else{
-			p_idx.end_idx = size.output.elem_num -1;//나누어 남는 부분은 부분은 마지막 thread가 모두 처리한다.
+			p_idx.pt[i].m_end_idx = size.output.elem_num - 1;//나누어 남는 부분은 부분은 마지막 thread가 모두 처리한다.
+			p_idx.pt[i].n_end_idx = size.vector.elem_num - 1;//나누어 남는 부분은 부분은 마지막 thread가 모두 처리한다.
 		}
-		
-		thr_id = pthread_create(*threads + i, NULL, &thread_main, (void *)&p_idx); //인자 전달부분에 계산 시작point를 전달
+		thr_id = pthread_create(*threads + i, NULL, func_p, (void *)&p_idx.pt[i]); //인자 전달부분에 계산 시작point를 전달
 		if (thr_id < 0){
 			perror("thread create error : ");
 
 		}
-		printf("Thread %d, %d\n", i, *(*threads+i));
+		//printf("Thread %d, %d\n", i, *(*threads+i));
 	}
 }
 
 void join_thread(pthread_t **threads, int thr_num, int *status){
 	int i;
 	for (i = 0; i < thr_num; i++){
-		pthread_join(*(*threads+i), NULL);
+		pthread_join(*(*threads + i), NULL);
 	}
 	//write
 	_fseeki64(fp.output, 0, SEEK_SET);
@@ -219,10 +243,23 @@ void join_thread(pthread_t **threads, int thr_num, int *status){
 	end_time_measurement(); //측정 종료
 }
 
-void *thread_main(void *p_idx){	buf.output = (double *)malloc(sizeof(double) * size.output.elem_num * 2);
-	int matrix_idx, vector_idx, output_idx;
-	matrix_idx = ((P_IDX *)p_idx)->start_idx;
-	for (output_idx = ((P_IDX *)p_idx)->start_idx; output_idx <= ((P_IDX *)p_idx)->start_idx; output_idx++){
+void *thread_read(void *pt){
+	
+
+	pthread_mutex_lock(&mutex);
+	_fseeki64(fp.matrix, ((PT *)pt)->m_start_idx * 4, SEEK_SET);
+	fread(&buf.matrix[(int)((PT *)pt)->m_start_idx * 4], sizeof(double), (((PT *)pt)->m_end_idx - ((PT *)pt)->m_start_idx + (double)1) * 4, fp.matrix);
+	printf("바보바보\n");
+	_fseeki64(fp.vector, ((PT *)pt)->n_start_idx * 2, SEEK_SET);
+	fread(&buf.matrix[(int)((PT *)pt)->n_start_idx * 2], sizeof(double), (((PT *)pt)->n_end_idx - ((PT *)pt)->n_start_idx + (double)1) * 2, fp.vector);
+	pthread_mutex_unlock(&mutex);
+	pthread_exit((void *)0);
+	return 0;
+}
+
+void *thread_calc(void *pt){	int matrix_idx, vector_idx, output_idx;
+	matrix_idx = ((PT *)pt)->m_start_idx;
+	for (output_idx = ((PT *)pt)->m_start_idx; output_idx <= ((PT *)pt)->m_end_idx; output_idx++){
 		for (vector_idx = 0; vector_idx < size.vector.elem_num; vector_idx++){
 			buf.output[2 * output_idx] += real_calc(buf.matrix[4 * matrix_idx + 2], buf.matrix[4 * matrix_idx + 3],
 				buf.vector[2 * vector_idx], buf.vector[2 * vector_idx + 1]);
--- 001. 세미나 자료/02.2015/03. 3차시/20150305_DuhyunHwang_MatrixVectorMultiplier.h
+++ 001. 세미나 자료/02.2015/03. 3차시/20150305_DuhyunHwang_MatrixVectorMultiplier.h
@@ -23,12 +23,19 @@ typedef struct BUF{
 	double *output;
 }BUF;
 
-typedef struct P_IDX{
-	double start_idx;
-	double end_idx;
-	double quotient;
-}P_IDX;
 
+typedef struct PT{	//mxn
+	double m_start_idx;
+	double m_end_idx;
+	double n_start_idx;
+	double n_end_idx;
+}PT;
+typedef struct P_IDX{
+	PT* pt;
+	double m_quotient;
+	double n_quotient;
+}P_IDX;
+ 
 void open_file(POINTERS *fp);
 void exit_process(POINTERS *fp);
 void find_size(POINTERS *fp, SIZES* size);
@@ -39,6 +46,7 @@ void free_alloc(BUF *buf);
 double real_calc(double A_real, double A_imag, double B_real, double B_imag);
 double imag_calc(double A_real, double A_imag, double B_real, double B_imag);
 
-void *thread_main(void *);
-void make_thread(pthread_t **threads, int thr_num);
+void *thread_calc(void *);
+void *thread_read(void *);
+void make_thread(pthread_t **threads, int thr_num, void *(*func_p)(void* ));
 void join_thread(pthread_t **threads, int thr_num, int *status);
